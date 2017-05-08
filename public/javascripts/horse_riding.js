c9dc7c8c7f3fbddecf954d3ebcebcc03e7b9d630
l2jong <asas1212>
2017-05-08 13:24:14 +0900

    init 2

--- public/javascripts/horse_riding.js
+++ public/javascripts/horse_riding.js
@@ -2,13 +2,34 @@ $(function () {
 
     var data = [];
 
-    var url = 'http://175.125.91.94/openapi/service/rest/meta/KFAstil';
+    var url = 'http://kradata.kra.co.kr:8082/service/api31/getOpenDataList';
+    var serviceKey = 'CCA597D83CDBAC9EB536A4BCD19ED5A4644F7B7E26565D335514BEF7D58F1E';
     $.ajax({
-        url: url
+        url: url + '?ServiceKey=' + serviceKey,
+        dataType: "json"
     })
         .done(function (response) {
-            console.log(response);
+            // console.log(response);
 
+            for (var i = 0; i < response.response.body.items.item.length; i++) {
+                data.push(response.response.body.items.item[i]);
+
+                var dataAddress = data[i].bsshAddr1 + " " + data[i].bsshAddr2;
+
+                var html = "";
+
+                html += '<tr class="gradeX">';
+                html += '<td>' + data[i].bsshNm + '</td>';
+                html += '<td>' + dataAddress + '</td>';
+                html += '<td>' + data[i].homepage + '</td>';
+                html += "</tr>";
+
+                $("#horseRiding").append(html);
+            }
+
+            $('#horseRiding').DataTable({
+                responsive: true
+            });
         });
 
 });
