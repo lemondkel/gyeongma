6ad4614e3e7e7c3d20b4ec789440caa4ae6b9d3e
l2jong <asas1212>
2017-05-11 08:55:55 +0900

    riding(승마장) 속도개선

--- public/javascripts/horse_jockey.js
+++ public/javascripts/horse_jockey.js
@@ -1,67 +1,26 @@
-$(function () {
+jkNoArr = jkNoArr.split(',');
+jkNameArr = jkNameArr.split(',');
+partArr = partArr.split(',');
+ageArr = ageArr.split(',');
+rcCntTArr = rcCntTArr.split(',');
+ord1CntTArr = ord1CntTArr.split(',');
 
-    var data = [];
 
-    var url = 'http://kradata.kra.co.kr:8082/service/api12/getOpenDataList';
-    var serviceKey = '57B72987563A5B0774417FF50FB1FB280A1CA6037CA8B1E6DF419C87BA341';
-    var meet = 1;
-    $.ajax({
-        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-        dataType: "json"
-    })
-        .done(function (response) {
+for (var i = 0; i < jkNoArr.length; i++) {
+    var html = "";
 
-            for (var i = 0; i < response.response.body.items.item.length; i++) {
-                data.push(response.response.body.items.item[i]);
-            }
+    html += '<tr class="gradeX">';
+    html += '<td>' + jkNoArr[i] + '</td>';
+    html += '<td>' + jkNameArr[i] + '</td>';
+    html += '<td>' + partArr[i] + '</td>';
+    html += '<td>' + ageArr[i] + '</td>';
+    html += '<td>' + rcCntTArr[i] + '</td>';
+    html += '<td>' + ord1CntTArr[i] + '</td>';
+    html += "</tr>";
 
-            meet = 2;
-            $.ajax({
-                url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-                dataType: "json"
-            })
-                .done(function (response) {
+    $("#horseJockey").append(html);
+}
 
-                    for (var i = 0; i < response.response.body.items.item.length; i++) {
-                        data.push(response.response.body.items.item[i]);
-                    }
-                    meet = 3;
-                    $.ajax({
-                        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-                        dataType: "json"
-                    })
-                        .done(function (response) {
-
-                            for (var i = 0; i < response.response.body.items.item.length; i++) {
-                                data.push(response.response.body.items.item[i]);
-                            }
-
-                            data.sort(function (obj1, obj2) {
-                                return obj1.jkName < obj2.jkName ?  -1
-                                    : (obj1.jkName > obj2.jkName ? 1 : 0);
-                            });
-
-                            for (var i = 0; i < data.length; i++) {
-                                var html = "";
-
-                                html += '<tr class="gradeX">';
-                                html += '<td>' + data[i].jkNo + '</td>';
-                                html += '<td>' + data[i].jkName + '</td>';
-                                html += '<td>' + data[i].part + '</td>';
-                                html += '<td>' + data[i].age + '</td>';
-                                html += '<td>' + data[i].rcCntT + '</td>';
-                                html += '<td>' + data[i].ord1CntT + '</td>';
-                                html += "</tr>";
-
-                                $("#horseJockey").append(html);
-                            }
-
-                            $('#horseJockey').DataTable({
-                                responsive: true
-                            });
-
-                        });
-                });
-        });
-
+$('#horseJockey').DataTable({
+    responsive: true
 });
--- routes/horseJockey.js
+++ routes/horseJockey.js
@@ -1,9 +1,138 @@
 var express = require('express');
 var router = express.Router();
+var cron = require('node-cron');
+var request = require('request');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser();
+
+var url = 'http://kradata.kra.co.kr:8082/service/api12/getOpenDataList';
+var serviceKey = '57B72987563A5B0774417FF50FB1FB280A1CA6037CA8B1E6DF419C87BA341';
+var jkNoArr = [];
+var jkNameArr = [];
+var partArr = [];
+var ageArr = [];
+var rcCntTArr = [];
+var ord1CntTArr = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('horse_jockey');
+router.get('/', function (req, res, next) {
+    if (jkNameArr.length === 0) {
+        ajaxFunction_jockey();
+    } else {
+        res.render('horse_jockey', {
+            jkNoArr : jkNoArr,
+            jkNameArr : jkNameArr,
+            partArr : partArr,
+            ageArr : ageArr,
+            rcCntTArr : rcCntTArr,
+            ord1CntTArr : ord1CntTArr
+        });
+    }
 });
+
+cron.schedule('* * 4 * *', function () {
+    console.log('[jockey]', 'starting a task every day / ' + new Date());
+
+    jkNoArr = [];
+    jkNameArr = [];
+    partArr = [];
+    ageArr = [];
+    rcCntTArr = [];
+    ord1CntTArr = [];
+
+    ajaxFunction_jockey();
+
+    console.log('[jockey]', 'ending a task every day / ' + new Date());
+});
+
+function ajaxFunction_jockey() {
+    var meet = 1;
+
+    request({
+        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+        method: "GET",
+        dataType: "json"
+    }, function (error, response, body) {
+        console.log('Reponse received', body);
+
+        parser.parseString(body, function (err, result) {
+            // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+            for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                var jkNo = result.response.body[0].items[0].item[i].jkNo;
+                var jkName = result.response.body[0].items[0].item[i].jkName;
+                var part = result.response.body[0].items[0].item[i].part;
+                var age = result.response.body[0].items[0].item[i].age;
+                var rcCntT = result.response.body[0].items[0].item[i].rcCntT;
+                var ord1CntT = result.response.body[0].items[0].item[i].ord1CntT;
+
+                jkNoArr.push(jkNo);
+                jkNameArr.push(jkName);
+                partArr.push(part);
+                ageArr.push(age);
+                rcCntTArr.push(rcCntT);
+                ord1CntTArr.push(ord1CntT);
+            }
+        });
+
+        meet = 2;
+
+        request({
+            url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+            method: "GET",
+            dataType: "json"
+        }, function (error, response, body) {
+            console.log('Reponse received', body);
+
+            parser.parseString(body, function (err, result) {
+                // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                    var jkNo = result.response.body[0].items[0].item[i].jkNo;
+                    var jkName = result.response.body[0].items[0].item[i].jkName;
+                    var part = result.response.body[0].items[0].item[i].part;
+                    var age = result.response.body[0].items[0].item[i].age;
+                    var rcCntT = result.response.body[0].items[0].item[i].rcCntT;
+                    var ord1CntT = result.response.body[0].items[0].item[i].ord1CntT;
+
+                    jkNoArr.push(jkNo);
+                    jkNameArr.push(jkName);
+                    partArr.push(part);
+                    ageArr.push(age);
+                    rcCntTArr.push(rcCntT);
+                    ord1CntTArr.push(ord1CntT);
+                }
+            });
+
+            meet = 3;
+
+            request({
+                url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+                method: "GET",
+                dataType: "json"
+            }, function (error, response, body) {
+                console.log('Reponse received', body);
+
+                parser.parseString(body, function (err, result) {
+                    // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                    for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                        var jkNo = result.response.body[0].items[0].item[i].jkNo;
+                        var jkName = result.response.body[0].items[0].item[i].jkName;
+                        var part = result.response.body[0].items[0].item[i].part;
+                        var age = result.response.body[0].items[0].item[i].age;
+                        var rcCntT = result.response.body[0].items[0].item[i].rcCntT;
+                        var ord1CntT = result.response.body[0].items[0].item[i].ord1CntT;
+
+                        jkNoArr.push(jkNo);
+                        jkNameArr.push(jkName);
+                        partArr.push(part);
+                        ageArr.push(age);
+                        rcCntTArr.push(rcCntT);
+                        ord1CntTArr.push(ord1CntT);
+                    }
+                });
+            });
+        });
+    });
+}
+
 
 module.exports = router;
