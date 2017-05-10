1b0e15b85907f498ee2025f5f5cf5be57656fdd3
l2jong <gkwkdlawoh@naver.com>
2017-05-11 00:04:28 +0900

    index, schedule 속도 최적화작업 완료 http 로딩 스크립트 서버로 이동

--- routes/index.js
+++ routes/index.js
@@ -5,63 +5,141 @@ var request = require('request');
 var xml2js = require('xml2js');
 var parser = xml2js.Parser();
 
+var url = 'http://kradata.kra.co.kr:8082/service/api40/getOpenDataList';
+var serviceKey = '72C194F6FEEAC8B323E85FCF1A3B68EE5617D4544A45621A4747C9D9B9C2';
+var dataDateArr = [];
+var rcNameArr = [];
+var rcGradeArr = [];
+var rcDistArr = [];
+var meetArr = [];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+
+    if (dataDateArr.length === 0)
+        ajaxFunction_index();
+
+    if (dataDateArr.length !== 0) {
+        res.render('index', {
+            title: 'Express',
+            dataDateArr: dataDateArr,
+            rcNameArr: rcNameArr,
+            rcGradeArr: rcGradeArr,
+            rcDistArr: rcDistArr,
+            meetArr: meetArr
+        });
+    }
 });
 
-cron.schedule('0 6 * * *', function () {
+cron.schedule('* * 4 * *', function () {
     console.log('[index]', 'starting a task every minute / ' + new Date());
+    dataDateArr = [];
+    rcNameArr = [];
+    rcGradeArr = [];
+    rcDistArr = [];
+    meetArr = [];
 
-    var url = 'http://kradata.kra.co.kr:8082/service/api40/getOpenDataList';
-    var serviceKey = '72C194F6FEEAC8B323E85FCF1A3B68EE5617D4544A45621A4747C9D9B9C2';
-    var meet = 1;
-    var arr = [];
-
-    request({
-        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-        method: "GET",
-        dataType: "json"
-    }, function (error, response, body) {
-        console.log('Reponse received', body);
-
-        var data = response.response.body.items.item;
-
-        for (var i = 0; i < data.length; i++)
-            arr.push(data[i]);
-
-    });
-
-    meet = 2;
-
-    request({
-        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-        method: "GET",
-        dataType: "json"
-    }, function (error, response, body) {
-        console.log('Reponse received', body);
-
-        var data = response.response.body.items.item;
-
-        for (var i = 0; i < data.length; i++)
-            arr.push(data[i]);
-
-    });
-
-    meet = 3;
-
-    request({
-        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
-        method: "GET",
-        dataType: "json"
-    }, function (error, response, body) {
-        console.log('Reponse received', body);
-
-        var data = response.response.body.items.item;
-
-        for (var i = 0; i < data.length; i++)
-            arr.push(data[i]);
-    });
+    ajaxFunction_index();
 });
+
+function ajaxFunction_index() {
+    var meet = 1;
+
+    request({
+        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+        method: "GET",
+        dataType: "json"
+    }, function (error, response, body) {
+        console.log('Reponse received', body);
+
+        parser.parseString(body, function (err, result) {
+            // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+            for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                var dataPgdate = result.response.body[0].items[0].item[i].pgDate.toString();
+                var dataYear = dataPgdate.substring(0, 4);
+                var dataMonth = dataPgdate.substring(4, 6);
+                var dataDay = dataPgdate.substring(6, 8);
+                var meet = 1;
+
+                var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+                var rcName = result.response.body[0].items[0].item[i].rcName;
+                var rcGrade = result.response.body[0].items[0].item[i].rcGrade;
+                var rcDist = result.response.body[0].items[0].item[i].rcDist;
+
+                dataDateArr.push(dataDate);
+                rcNameArr.push(rcName);
+                rcGradeArr.push(rcGrade);
+                rcDistArr.push(rcDist);
+                meetArr.push(meet);
+            }
+        });
+
+        meet = 2;
+
+        request({
+            url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+            method: "GET",
+            dataType: "json"
+        }, function (error, response, body) {
+            console.log('Reponse received', body);
+
+            parser.parseString(body, function (err, result) {
+                // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                    var dataPgdate = result.response.body[0].items[0].item[i].pgDate.toString();
+                    var dataYear = dataPgdate.substring(0, 4);
+                    var dataMonth = dataPgdate.substring(4, 6);
+                    var dataDay = dataPgdate.substring(6, 8);
+                    var meet = 2;
+
+                    var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+                    var rcName = result.response.body[0].items[0].item[i].rcName;
+                    var rcGrade = result.response.body[0].items[0].item[i].rcGrade;
+                    var rcDist = result.response.body[0].items[0].item[i].rcDist;
+
+                    dataDateArr.push(dataDate);
+                    rcNameArr.push(rcName);
+                    rcGradeArr.push(rcGrade);
+                    rcDistArr.push(rcDist);
+                    meetArr.push(meet);
+                }
+            });
+
+            meet = 3;
+
+            request({
+                url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet,
+                method: "GET",
+                dataType: "json"
+            }, function (error, response, body) {
+                console.log('Reponse received', body);
+
+                parser.parseString(body, function (err, result) {
+                    // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                    for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                        var dataPgdate = result.response.body[0].items[0].item[i].pgDate.toString();
+                        var dataYear = dataPgdate.substring(0, 4);
+                        var dataMonth = dataPgdate.substring(4, 6);
+                        var dataDay = dataPgdate.substring(6, 8);
+                        var meet = 3;
+
+                        var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+                        var rcName = result.response.body[0].items[0].item[i].rcName;
+                        var rcGrade = result.response.body[0].items[0].item[i].rcGrade;
+                        var rcDist = result.response.body[0].items[0].item[i].rcDist;
+
+                        dataDateArr.push(dataDate);
+                        rcNameArr.push(rcName);
+                        rcGradeArr.push(rcGrade);
+                        rcDistArr.push(rcDist);
+                        meetArr.push(meet);
+
+                        console.log(meetArr.length);
+                    }
+                });
+            });
+        });
+    });
+}
 
 module.exports = router;
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,9 +1,169 @@
 var express = require('express');
 var router = express.Router();
+var cron = require('node-cron');
+var request = require('request');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser();
+
+var url = 'http://kradata.kra.co.kr:8082/service/api72/getOpenDataList';
+var serviceKey = 'CCA597D83CDBAC9EB536A4BCD19ED5A4644F7B7E26565D335514BEF7D58F1E';
+var dataDateArr = [];
+var meetArr = [];
+var rcNoArr = [];
+var meetTypeArr = [];
+var rcDateArr = [];
+var rcNameArr = [];
+var rankArr = [];
+var rcDistArr = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('horse_race_total_schedule');
+router.get('/', function (req, res, next) {
+
+    if (dataDateArr.length === 0)
+        ajaxFunction_schedule();
+
+    if (dataDateArr.length !== 0) {
+        res.render('horse_race_total_schedule', {
+            dataDateArr: dataDateArr,
+            meetArr: meetArr,
+            rcNoArr: rcNoArr,
+            meetTypeArr: meetTypeArr,
+            rcDateArr: rcDateArr,
+            rcNameArr: rcNameArr,
+            rankArr: rankArr,
+            rcDistArr: rcDistArr
+        });
+    }
 });
+
+cron.schedule('* * 4 * *', function () {
+    console.log('[schedule]', 'starting a task every minute / ' + new Date());
+    rcDateArr = [];
+    dataDateArr = [];
+    meetArr = [];
+    rcNoArr = [];
+    meetTypeArr = [];
+    rcNameArr = [];
+    rankArr = [];
+    rcDistArr = [];
+
+    ajaxFunction_schedule();
+});
+
+function ajaxFunction_schedule() {
+    var meet = 1;
+    var rc_year = 2017;
+
+    request({
+        url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet + "&rc_year=" + rc_year,
+        method: "GET",
+        dataType: "json"
+    }, function (error, response, body) {
+        console.log('Reponse received');
+
+        parser.parseString(body, function (err, result) {
+            // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+            for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                var rcDate = result.response.body[0].items[0].item[i].rcDate.toString();
+                var dataYear = rcDate.substring(0, 4);
+                var dataMonth = rcDate.substring(4, 6);
+                var dataDay = rcDate.substring(6, 8);
+                var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+
+                var meetType = 1;
+                var meet = result.response.body[0].items[0].item[i].meet;
+                var rcName = result.response.body[0].items[0].item[i].rcName;
+                var rcDist = result.response.body[0].items[0].item[i].rcDist;
+                var rcNo = result.response.body[0].items[0].item[i].rcNo;
+                var rank = result.response.body[0].items[0].item[i].rank;
+
+                rcDateArr.push(rcDate);
+                dataDateArr.push(dataDate);
+                meetTypeArr.push(meetType);
+                meetArr.push(meet);
+                rcNameArr.push(rcName);
+                rcDistArr.push(rcDist);
+                rcNoArr.push(rcNo);
+                rankArr.push(rank);
+            }
+        });
+
+        meet = 2;
+
+        request({
+            url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet + "&rc_year=" + rc_year,
+            method: "GET",
+            dataType: "json"
+        }, function (error, response, body) {
+            console.log('Reponse received!');
+
+            parser.parseString(body, function (err, result) {
+                // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                    var rcDate = result.response.body[0].items[0].item[i].rcDate.toString();
+                    var dataYear = rcDate.substring(0, 4);
+                    var dataMonth = rcDate.substring(4, 6);
+                    var dataDay = rcDate.substring(6, 8);
+                    var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+
+                    var meetType = 1;
+                    var meet = result.response.body[0].items[0].item[i].meet;
+                    var rcName = result.response.body[0].items[0].item[i].rcName;
+                    var rcDist = result.response.body[0].items[0].item[i].rcDist;
+                    var rcNo = result.response.body[0].items[0].item[i].rcNo;
+                    var rank = result.response.body[0].items[0].item[i].rank;
+
+                    rcDateArr.push(rcDate);
+                    dataDateArr.push(dataDate);
+                    meetTypeArr.push(meetType);
+                    meetArr.push(meet);
+                    rcNameArr.push(rcName);
+                    rcDistArr.push(rcDist);
+                    rcNoArr.push(rcNo);
+                    rankArr.push(rank);
+                }
+            });
+
+            meet = 3;
+
+            request({
+                url: url + '?ServiceKey=' + serviceKey + '&meet=' + meet + "&rc_year=" + rc_year,
+                method: "GET",
+                dataType: "json"
+            }, function (error, response, body) {
+                console.log('Reponse received!');
+
+                parser.parseString(body, function (err, result) {
+                    // console.log(JSON.stringify(result.response.body[0].items[0].item[0]));
+                    for (var i = 0; i < result.response.body[0].items[0].item.length; i++) {
+                        var rcDate = result.response.body[0].items[0].item[i].rcDate.toString();
+                        var dataYear = rcDate.substring(0, 4);
+                        var dataMonth = rcDate.substring(4, 6);
+                        var dataDay = rcDate.substring(6, 8);
+                        var dataDate = dataYear + "-" + dataMonth + "-" + dataDay;
+
+                        var meetType = 1;
+                        var meet = result.response.body[0].items[0].item[i].meet;
+                        var rcName = result.response.body[0].items[0].item[i].rcName;
+                        var rcDist = result.response.body[0].items[0].item[i].rcDist;
+                        var rcNo = result.response.body[0].items[0].item[i].rcNo;
+                        var rank = result.response.body[0].items[0].item[i].rank;
+
+                        rcDateArr.push(rcDate);
+                        dataDateArr.push(dataDate);
+                        meetTypeArr.push(meetType);
+                        meetArr.push(meet);
+                        rcNameArr.push(rcName);
+                        rcDistArr.push(rcDist);
+                        rcNoArr.push(rcNo);
+                        rankArr.push(rank);
+
+                        console.log('[schedule]', 'ending a task every minute / ' + new Date());
+                    }
+                });
+            });
+        });
+    });
+}
 
 module.exports = router;
